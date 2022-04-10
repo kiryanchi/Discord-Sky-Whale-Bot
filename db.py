350e83d08c90dfbc443c01f78933f44c56c62284
kiryanchi <kiryanchi@naver.com>
2022-04-10 15:24:40 +0900

    DB 컨트롤러

--- db.py
+++ db.py
@@ -7,10 +7,46 @@ class DB:
         self.cursor = self.con.cursor()
 
     def init(self):
-        self.cursor.execute("CREATE TABLE server(Date text, ID int)")
-        self.cursor.execute("CREATE TABLE muisc(ID int, Chanel int, PlayList int, Player int, Button int)")
+        self.cursor.execute("CREATE TABLE server(ID int)")
+        self.cursor.execute(
+            "CREATE TABLE muisc(ID int, Chanel int, \
+                PlayList int, Player int, Button int)"
+        )
 
+    def close(self):
+        self.con.close()
 
-if __name__ == '__main__':
-    db = DB('muple.db')
-    db.init()
+    def insert(self, table, *values):
+        try:
+            assert len(values) > 0, "VALUES 값이 필요합니다."
+
+            if table == "music":
+                assert (
+                    len(values) == 5
+                ), "music 테이블은 id, channel, playlist, player, button이 필요합니다."
+            elif table == "server":
+                assert len(values) == 1, "server 테이블은 id이 필요합니다."
+            else:
+                assert table == "music" or table == "server", f"{table}은 존재하지 않습니다."
+        except AssertionError as e:
+            print(e)
+        else:
+            items = " ".join(values)
+            query_string = f"INSERT INTO {table} VALUES ({items})"
+            self.cursor.execute(query_string)
+            self.con.commit()
+
+    def fetch(self, table, *values):
+        try:
+            assert len(values) > 0, "VALUES 값이 필요합니다."
+            assert table == "music" or table == "server", f"{table}은 존재하지 않습니다."
+        except AssertionError as e:
+            print(e)
+        else:
+            self.cursor.execute(f"SELECT {values} FROM {table}")
+
+    def insert_server(self, id):
+        self.cursor.execute(f"INSERT INTO server VALUES({id})")
+
+    def disconnect(self):
+        self.con.close()
