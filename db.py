dbee07876f5ad8347d8778c9056803df400b9afc
kiryanchi <kiryanchi@naver.com>
2022-04-12 11:49:49 +0900

    DB 구조 수정

--- db.py
+++ db.py
@@ -4,14 +4,12 @@ import sqlite3
 class DB:
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):
-            print("DB 연결 됨")
             cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
         self.con = sqlite3.connect("muple.db")
         self.cursor = self.con.cursor()
-        print("싱글 톤 DB 연결 됨")
 
     def save(self, query_string, param=None):
         if param:
@@ -24,16 +22,16 @@ class DB:
         self.con.close()
         print("DB 성공적으로 해제 됨")
 
-    def insert_music_channel(
-        self, server_id, channel_id, user_id, playlist_id, button_id
-    ):
-        query_string = f"INSERT INTO music_channel VALUES ({server_id}, {channel_id}, {user_id}, {playlist_id}, {button_id})"
+    def insert_music_channel(self, guild_id, channel_id, playlist_id, button_id):
+        query_string = f"INSERT INTO music_channel VALUES ({guild_id}, {channel_id}, {playlist_id}, {button_id})"
         self.save(query_string)
 
-    def is_exist_music_channel(self, server_id):
-        query_string = "SELECT id FROM music_channel WHERE server_id='%s'" % server_id
-        self.save(query_string)
+    def select_music_channel(self, guild_id):
+        query_string = "SELECT * FROM music_channel WHERE guild_id='%s'" % guild_id
+        self.cursor.execute(query_string)
 
-    def delete_music_channel(self, server_id):
-        query_string = "DELETE FROM music_channel WHERE server_id='%s'" % server_id
+        return self.cursor.fetchall()
+
+    def delete_music_channel(self, guild_id):
+        query_string = "DELETE FROM music_channel WHERE guild_id='%s'" % guild_id
         self.save(query_string)
