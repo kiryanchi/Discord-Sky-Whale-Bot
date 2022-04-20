aca97392043b9ea8eaa92147f2c2228e7b8e13d9
kiryanchi <kiryanchi@naver.com>
2022-04-20 12:53:08 +0900

    플레이리스트 기본 틀 완성

--- Cogs/Playlist.py
+++ Cogs/Playlist.py
@@ -1,5 +1,9 @@
 import discord
+import asyncio
 from discord.ext import commands
+
+from src.playlist.embed import Embed
+from src.playlist.player import Player
 
 
 class Playlist(commands.Cog):
@@ -8,11 +12,110 @@ class Playlist(commands.Cog):
         self.db = bot.db
         self.guilds = {}
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        servers = self.db.select_all_music_channel()
+        if servers:
+            for guild_id, channel_id in servers:
+                channel = self.bot.get_channel(channel_id)
+                await self._create_playlist(channel)
+        print("[INFO] 음악봇 초기화 완료")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.channel in self.bot.music_channel_list:
+            return
+
+        if message.content == f"{self.bot.command_prefix}초기화":
+            await self.bot.process_command(message)
+            return
+
+        if message.content.startswith(self.bot.command_prefix):
+            pass
+
+        if message.author.voice is None:
+            await message.channel.send("음성 채널에 들어가서 사용해주세요", delete_after=3)
+            await asyncio.sleep(3)
+            return await message.delete()
+
+        if "youtube.com" in message.content:
+            await message.channel.send("youtube 주소 검색 기능은 구현중입니다...", delete_after=3)
+            return
+
+        print(f"[INFO] {message.content} 검색")
+
     @commands.command(neme="초기화", help="이 채널을 음악봇이 사용합니다.")
-    async def start(self):
+    @commands.has_permissions(administrator=True)
+    async def start(self, ctx):
+        async def callback(interaction):
+            if interaction.user == ctx.author:
+                if self.db.select_music_channel(ctx.guild.id):
+                    self.db.delete_music_channel(ctx.guild.id)
+                self.db.insert_music_channel(ctx.guild.id, ctx.channel.id)
+            await self._create_playlist(ctx.channel)
+
+        embed, components = Embed.start()
+        components = [
+            self.bot.components_manager.add_callback(component, callback)
+            for component in components
+        ]
+
+        await ctx.send(embed=embed, components=components, delete_after=5)
+
+    async def _create_playlist(self, channel):
+        async def callback(interaction):
+            actions = {
+                "pause": self._pause,
+                "resume": self._resume,
+                "shuffle": self._shuffle,
+                "skip": self._skip,
+                "prev": self._prev,
+                "next": self._next,
+                "first": self._first,
+                "last": self._last,
+            }
+            await actions[interaction.custom_id]()
+
+        embed, components_list = Embed.playlist()
+
+        components = [
+            [
+                self.bot.components_manager.add_callback(component, callback)
+                for component in components
+            ]
+            for components in components_list
+        ]
+
+        self.guilds[channel.guild] = {channel: Player(channel)}
+        self.bot.music_channel_list.append(channel)
+
+        await channel.send(embed=embed, components=components)
+
+    async def _first(self, interaction):
         pass
 
-    async def _create_playlist(self):
+    async def _next(self, interaction):
+        pass
+
+    async def _last(self, interaction):
+        pass
+
+    async def _pause(self, interaction):
+        pass
+
+    async def _prev(self, interaction):
+        pass
+
+    async def _resume(self, interaction):
+        pass
+
+    async def _shuffle(self, interaction):
+        pass
+
+    async def _skip(self, interaction):
         pass
 
 
---
+++ src/playlist/embed.py
@@ -0,0 +1,112 @@
+import re
+import discord
+from discord_components import Button, ButtonStyle
+
+COLOR = 0x8AFDFD
+URL = "https://cdn.discordapp.com/attachments/963347486720798770/963347758067093544/unknown.png"
+CURRENT_SONG_NAME: str = "현재 재생중인 노래"
+NEXT_SONGS_NAME: str = "대기중인 노래"
+
+INIT_MSG = "```ansi\n[1;36m하늘 고래[0m가[1;34m 하늘[0m을 [1;35m향유[0m하기 시작했어요\n```"
+NUM_OF_SEARCH = 9
+
+
+class Embed:
+    def wrap(self, text):
+        def is_korean(char):
+            hangul = re.compile("[^ㄱ-ㅣ가-힣]+")
+            result = hangul.sub("", char)
+            return result != ""
+
+        word_cnt = 0
+        result_text = ""
+        for char in text:
+            if word_cnt > 42:
+                result_text += "..."
+                break
+
+            if is_korean(char):
+                word_cnt += 2
+            else:
+                word_cnt += 1
+            result_text += char
+        return result_text
+
+    @staticmethod
+    def start(self):
+        embed = (
+            discord.Embed(title="하늘 고래가 이 곳을 떠다니고 싶어합니다.", color=COLOR)
+            .set_image(url=URL)
+            .set_footer(text="수락을 누르면 이 채널을 음악 채널로 사용합니다.")
+        )
+
+        components = [
+            Button(style=ButtonStyle.green, label="여기에 날아다니렴", custom_id="fly")
+        ]
+
+        return embed, components
+
+    @staticmethod
+    def search(message, info):
+        NUM_OF_SEARCH = 9
+        embed = discord.Embed(
+            title=f"{message.content} 검색 결과", color=COLOR
+        ).set_thumbnail(url=URL)
+
+        for i in range(len(info)):
+            embed.add_field(
+                name=f"{i+1:2d}번\t({info[i]['duration']}) {info[i]['channel']['name']}",
+                value=f"제목: {info[i]['title']}",
+                inline=False,
+            )
+
+        components = []
+
+        for i in range(NUM_OF_SEARCH // 5 + 1):
+            component = []
+            for j in range(1, 6):
+                if i * 5 + j - 1 == NUM_OF_SEARCH:
+                    break
+                component.append(
+                    Button(label=i * 5 + j, custom_id=f"{message.content}{i*5 + j}")
+                )
+            components.append(component)
+        components[-1].append(
+            Button(
+                style=ButtonStyle.red, label="Cancel", custom_id=f"{message.content}c"
+            )
+        )
+
+        return embed, components
+
+    @staticmethod
+    def playlist():
+        current_song_msg = "current_song_msg"
+        next_songs_msg = "nex_songs_msg"
+
+        embed = (
+            discord.Embed(title="\u2000" * 5 + "Sky Whale", color=COLOR)
+            .set_image(url=URL)
+            .add_field(name=CURRENT_SONG_NAME, value=current_song_msg, inline=False)
+            .add_field(name=NEXT_SONGS_NAME, value=next_songs_msg, inline=False)
+            .set_footer(text="노래를 검색해서 추가하세요.")
+        )
+
+        components = [
+            [
+                Button(style=ButtonStyle.red, label="||", custom_id="pause"),
+                Button(style=ButtonStyle.green, label="▶", custom_id="resume"),
+                Button(style=ButtonStyle.blue, label="skip", custom_id="skip"),
+                Button(style=ButtonStyle.grey, label="↻", custom_id="shuffle"),
+                Button(style=ButtonStyle.grey, label="?", custom_id="help"),
+            ],
+            [
+                Button(style=ButtonStyle.grey, label="<", custom_id="prev_page"),
+                Button(style=ButtonStyle.grey, label=">", custom_id="next_page"),
+                Button(style=ButtonStyle.grey, label="<<", custom_id="first_page"),
+                Button(style=ButtonStyle.grey, label=">>", custom_id="last_page"),
+                Button(style=ButtonStyle.grey, label="Youtube", custom_id="yt"),
+            ],
+        ]
+
+        return embed, components
