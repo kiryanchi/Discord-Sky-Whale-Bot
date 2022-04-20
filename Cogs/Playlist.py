7da38b02c1d188a001a0c82672d322aec11f50b9
kiryanchi <kiryanchi@naver.com>
2022-04-20 16:40:56 +0900

    [test] 노래 재생 테스트

--- Cogs/Playlist.py
+++ Cogs/Playlist.py
@@ -141,8 +141,102 @@ class Song:
 
 
 class Player:
-    def __init__(self, channel):
-        self.channel = channel
+    def __init__(self, playlist_channel):
+        self.FFMPEG_OPTIONS = {
+            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+            "options": "-vn",
+        }
+        self._playing = False
+        self.playlist_channel = playlist_channel
+        self._playlist_msg = None
+        self._songs = {"current": None, "next": []}
+        self._voice = {"channel": None, "client": None}
+
+    @property
+    def playlist_msg(self):
+        return self._playlist_msg
+
+    def set_playlist_msg(self, msg):
+        self._playlist_msg = msg
+
+    def pause(self):
+        if self._voice["client"] and not self.voice["client"].is_paused():
+            self._voice["client"].pause()
+
+    def resume(self):
+        if self._voice["client"] and self.voice["client"].is_paused():
+            self._voice["client"].resume()
+
+    def skip(self):
+        pass
+
+    async def play(self, message, song):
+        if self._voice["client"] is None:
+            await self._join(message.author.voice.channel)
+
+        if self._playing:
+            self._add_song(song)
+            if not self._same_voice_channel(message.author.voice.channel):
+                return await message.channel.send(
+                    "노래를 추가했어요! 하지만 채널이 다르신 것 같아요.. 노래를 듣고싶다면 제가 있는 채널에 들어와주세요!"
+                )
+
+        if not self._same_voice_channel(message.author.voice.channel):
+            await self._move(message.author.voice.channel)
+
+        await self._play_song(song)
+
+    def _add_song(self, song):
+        self._songs["next"].append(song)
+
+    async def _check_queue(self):
+        if not self._songs["next"]:
+            self._playing = False
+            self._songs["current"] = None
+            await self._leave()
+            return
+        self._voice["client"].stop()
+        await self._play_song(self._get_song())
+
+    def _get_song(self):
+        return self._songs["next"].pop(0)
+
+    async def _join(self, voice_channel):
+        self._voice["channel"] = voice_channel
+        self._voice["client"] = await voice_channel.connect()
+
+    async def _leave(self):
+        self._voice["client"] = await self._voice["client"].disconnect()
+        self._voice["channel"] = None
+
+    async def _move(self, voice_channel):
+        self._voice["channel"] = voice_channel
+        self._voice["client"] = await self.voice["clinet"].move_to(voice_channel)
+
+    async def _play_song(self, song):
+        self._playing = True
+        self._songs["current"] = song
+
+        try:
+            self._voice["client"].play(
+                discord.PCMVolumeTransformer(
+                    discord.FFmpegPCMAudio(song.url, **self.FFMPEG_OPTIONS)
+                ),
+                after=lambda error: asyncio.get_event_loop().create_task(
+                    self._check_queue()
+                ),
+            )
+            self.pause()
+            await asyncio.sleep(1)
+            self.resume()
+        except discord.opus.OpusNotLoaded:
+            print(f"{song.title} play")
+            await asyncio.sleep(10)
+            print("play done")
+            await self._check_queue()
+
+    def _same_voice_channel(self, voice_channel):
+        return self._voice["channel"] == voice_channel
 
 
 class Youtube:
@@ -169,7 +263,7 @@ class Playlist(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.db
-        self.guilds = {}
+        self.players = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -213,8 +307,10 @@ class Playlist(commands.Cog):
         if "youtu.be" in message.content:
             link = message.content
 
-        song = Youtube.extract_info(link=link)
-        print(song)
+        await self.players[message.guild].play(
+            message=message, song=Youtube.extract_info(link=link)
+        )
+
         await message.delete()
 
     @commands.command(neme="초기화", help="이 채널을 음악봇이 사용합니다.")
@@ -265,10 +361,14 @@ class Playlist(commands.Cog):
             for components in components_list
         ]
 
-        self.guilds[channel.guild] = {channel: Player(channel)}
+        if channel in self.bot.music_channel_list:
+            self.bot.music_channel_list.remove(channel)
         self.bot.music_channel_list.append(channel)
 
-        await channel.send(embed=embed, components=components)
+        player = Player(playlist_channel=channel)
+        playlist_msg = await channel.send(embed=embed, components=components)
+        player.set_playlist_msg(playlist_msg)
+        self.players[channel.guild] = player
 
     async def _first(self, interaction):
         pass
